d5474067b6661cf00568345180df26e88968f81b
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-28 09:34:19 -0300

    Creating Timer script

---
+++ space-battle/Assets/Scripts/Networking/Timer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+
+public class Timer : MonoBehaviourPunCallbacks
+{
+    ExitGames.Client.Photon.Hashtable customPropertie;
+    private bool hasStarted = false;
+    private float time;
+    [SerializeField] float initialTime = 180f;
+    [SerializeField] Text timerText;
+
+    private void Update()
+    {
+        if (hasStarted)
+        {
+            UpdateTimer();
+        }
+    }
+
+    public void SetTimer()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            time = initialTime;
+            customPropertie = new ExitGames.Client.Photon.Hashtable()
+            {
+                {"Timer",time}
+            };
+
+            PhotonNetwork.CurrentRoom.SetCustomProperties(customPropertie);
+        }
+
+        hasStarted = true;
+    }
+
+    public void UpdateTimer()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            time -= Time.deltaTime;
+            ExitGames.Client.Photon.Hashtable auxiliaryCustomPropertie = PhotonNetwork.CurrentRoom.CustomProperties;
+            auxiliaryCustomPropertie.Remove("Timer");
+            auxiliaryCustomPropertie.Add("Timer", time);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(auxiliaryCustomPropertie);
+        }
+    }
+
+    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    {
+        time = (float)propertiesThatChanged["Timer"];
+        SetText();
+    }
+
+    private void SetText()
+    {
+        if (time > 0)
+        {
+            int minutes = (int)time / 60;
+            int seconds = (int)time % 60;
+
+            timerText.text = minutes + ":" + seconds.ToString("D2");
+        }
+    }
+}
