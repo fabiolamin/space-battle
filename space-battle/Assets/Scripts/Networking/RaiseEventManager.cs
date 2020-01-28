56446023c3f77103909ed432b0516bdfa057f094
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-28 09:34:54 -0300

    Creating RaiseEventManager script

---
+++ space-battle/Assets/Scripts/Networking/RaiseEventManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class RaiseEventManager : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public void Create(byte raiseEventCode, object content)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            RaiseEventOptions raiseEventOptions = new RaiseEventOptions()
+            {
+                Receivers = ReceiverGroup.All
+            };
+            SendOptions sendOptions = new SendOptions { Reliability = true };
+
+            PhotonNetwork.RaiseEvent(raiseEventCode, content, raiseEventOptions, sendOptions);
+        }
+    }
+}
