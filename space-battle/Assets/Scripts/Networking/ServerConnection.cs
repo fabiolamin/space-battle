687aa28319d98e6cc6a488b782bce8e4dce299ad
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-28 09:33:07 -0300

    Creating ServerConnection script

---
+++ space-battle/Assets/Scripts/Networking/ServerConnection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class ServerConnection : MonoBehaviourPunCallbacks
+{
+    private PlayerNetworkingSettings playerNetworkingSettings;
+    [SerializeField] private UIPanelManager uiPanelManager;
+
+    private void Awake()
+    {
+        playerNetworkingSettings = GetComponent<PlayerNetworkingSettings>();
+    }
+
+    public void ConnectToServer()
+    {
+        playerNetworkingSettings.SetNickName();
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        uiPanelManager.EnableStartPanel();
+    }
+}
