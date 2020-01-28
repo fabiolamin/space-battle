b65a20fe52a4f198857ac5f5d787d8e6714d9429
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-28 09:32:19 -0300

    Creating PlayerNetworkingSettings script

---
+++ space-battle/Assets/Scripts/Networking/PlayerNetworkingSettings.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerNetworkingSettings : MonoBehaviour
+{
+    private int index;
+    [SerializeField] private InputField playerName;
+
+    public void ChooseSpaceShip(int value)
+    {
+        index = value;
+    }
+
+    public void SetNickName()
+    {
+        if (IsInputFieldEmpty())
+        {
+            PhotonNetwork.NickName = "Player " + Random.Range(100, 999);
+        }
+
+        else
+        {
+            PhotonNetwork.NickName = playerName.text;
+        }
+    }
+
+    private bool IsInputFieldEmpty()
+    {
+        return playerName == null;
+    }
+}
