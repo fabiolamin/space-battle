7ed641d7bb41ef3d18accdc8bbe1ffc8202198f7
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-29 13:32:15 -0300

    Creating PlayerMovement script

---
+++ space-battle/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    private Vector2 screenBoundaries;
+    private float spriteWidth;
+    private float spriteHeight;
+    [SerializeField] float speed = 200f;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        screenBoundaries = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        SetBoundaries();
+    }
+
+    private void Move()
+    {
+        Vector2 newPosition = playerRigidbody.position;
+        float angle = playerRigidbody.rotation;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition.y += speed * Time.fixedDeltaTime;
+            angle = 0;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition.y -= speed * Time.fixedDeltaTime;
+            angle = -180;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition.x += speed * Time.fixedDeltaTime;
+            angle = -90;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition.x -= speed * Time.fixedDeltaTime;
+            angle = 90;
+        }
+
+        playerRigidbody.MovePosition(newPosition);
+        playerRigidbody.MoveRotation(angle);
+    }
+
+    private void SetBoundaries()
+    {
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, 0f + spriteWidth, screenBoundaries.x - spriteWidth);
+        position.y = Mathf.Clamp(position.y, 0f + spriteHeight, screenBoundaries.y - spriteHeight);
+        transform.position = position;
+    }
+}
