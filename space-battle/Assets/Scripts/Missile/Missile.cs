a1a881abd80cb0ef704db17b4a5e95d7b5d50091
Fábio Lamin <fabio_lamin@hotmail.com>
2020-01-30 18:55:43 -0300

    Starting to implement the player shooting

---
+++ space-battle/Assets/Scripts/Missile/Missile.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    private float timeAuxiliary;
+    private Rigidbody2D missileRigidbody;
+    [SerializeField] float shootingForce = 1000f;
+    [SerializeField] float timeToDisable = 2f;
+
+    private void Awake()
+    {
+        timeAuxiliary = timeToDisable;
+        missileRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(IsEnabled())
+        {
+            if(IsCountdownDone())
+            {
+                Disable();
+                timeToDisable = timeAuxiliary;
+            }
+        }
+    }
+
+    private bool IsEnabled()
+    {
+        return gameObject.activeSelf;
+    }
+
+    private bool IsCountdownDone()
+    {
+        timeToDisable -= Time.deltaTime;
+        if(timeToDisable <= 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        missileRigidbody.AddForce(direction * shootingForce);
+    }
+
+    public void Enable()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void SetPosition(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+    }
+
+    public void SetRotation(Quaternion newRotation)
+    {
+        transform.rotation = newRotation;
+    }
+}
---
+++ space-battle/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerShooting : MonoBehaviour
+{
+    private PhotonView photonView;
+    private Missile[] missiles;
+    private int position = 0;
+    [SerializeField] private Missile missile;
+    [SerializeField] private int numberOfMissiles = 5;
+    [SerializeField] private Transform missileSpawn;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+        missiles = new Missile[numberOfMissiles];
+        if (photonView.IsMine)
+        {
+            photonView.RPC("InstantiateMissiles", RpcTarget.AllBuffered);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && photonView.IsMine)
+        {
+            photonView.RPC("SetShooting", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    private void InstantiateMissiles()
+    {
+        for (int position = 0; position < numberOfMissiles; position++)
+        {
+            missiles[position] = Instantiate(missile, missileSpawn.position, Quaternion.identity).GetComponent<Missile>();
+            missiles[position].Disable();
+        }
+    }
+
+    [PunRPC]
+    private void SetShooting()
+    {
+        if (IsAnAvailablePosition())
+        {
+            SetMissile();
+            Shoot();
+            position++;
+        }
+        else
+        {
+            position = 0;
+        }
+    }
+
+    private bool IsAnAvailablePosition()
+    {
+        return position < missiles.Length;
+    }
+
+    private void SetMissile()
+    {
+        missiles[position].SetPosition(missileSpawn.position);
+        missiles[position].SetRotation(transform.rotation);
+        missiles[position].Enable();
+    }
+
+    private void Shoot()
+    {
+        missiles[position].Move(transform.up);
+    }
+}
