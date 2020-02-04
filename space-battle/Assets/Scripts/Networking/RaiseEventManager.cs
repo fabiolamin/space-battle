ce9badc42e831c4a872ede68dcfdb87e4c08fb90
Fábio Lamin <fabio_lamin@hotmail.com>
2020-02-04 15:50:32 -0300

    Update RaiseEventManager.cs

--- space-battle/Assets/Scripts/Networking/RaiseEventManager.cs
+++ space-battle/Assets/Scripts/Networking/RaiseEventManager.cs
@@ -5,27 +5,14 @@ using ExitGames.Client.Photon;
 
 public class RaiseEventManager : MonoBehaviour
 {
-    public void OnEnable()
-    {
-        PhotonNetwork.AddCallbackTarget(this);
-    }
-
-    public void OnDisable()
-    {
-        PhotonNetwork.RemoveCallbackTarget(this);
-    }
-
     public void Create(byte raiseEventCode, object content)
     {
-        if (PhotonNetwork.IsMasterClient)
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions()
         {
-            RaiseEventOptions raiseEventOptions = new RaiseEventOptions()
-            {
-                Receivers = ReceiverGroup.All
-            };
-            SendOptions sendOptions = new SendOptions { Reliability = true };
+            Receivers = ReceiverGroup.All
+        };
+        SendOptions sendOptions = new SendOptions { Reliability = true };
 
-            PhotonNetwork.RaiseEvent(raiseEventCode, content, raiseEventOptions, sendOptions);
-        }
+        PhotonNetwork.RaiseEvent(raiseEventCode, content, raiseEventOptions, sendOptions);
     }
 }
